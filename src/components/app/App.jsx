4c21ac36f6f503dce71a6055a4f71c447351ef08
JC Guerrero <JCystems@gMail.com>
2022-07-11 14:08:47 -0500

    feat(SMART on FHIR)
    Had to refactor to use react JS 17

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import { PatientVisualizer } from 'fhir-visualizers'
 
-export function App () {
+export default function App (props) {
+  console.debug('App', props)
+  const client = props.client
+
+  const [patient, setPatient] = useState(null)
+  useEffect(() => {
+    client
+      .patient.read()
+      .then((patient) => {
+        console.debug('patient', patient)
+        setPatient(patient)
+        // this.forceUpdate() // FIXME XXX
+      })
+  })
+
   return (
     <div>
-      Home
+      {
+        patient
+          ? <PatientVisualizer patient={patient} />
+          : <h1>Loading</h1>
+      }
     </div>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import FHIR from 'fhirclient'
+import App from './components/app/App'
 import './index.css'
-import { App } from './components/app/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const divHTMLElement = document.getElementById('app')
+console.info('index.jsx')
 
-ReactDOM
-  .createRoot(divHTMLElement)
-  .render(<App />)
+FHIR
+  .oauth2
+  .init({
+    clientId: '20560ea5-f224-4658-b667-4e6bab935c85',
+    scope: 'launch/patient openid profile'
+  })
+  .then(client => {
+    console.debug('client', client)
+    const divHTMLElement = document.getElementById('app')
+    ReactDOM.render(<App client={client} />, divHTMLElement)
+  })
